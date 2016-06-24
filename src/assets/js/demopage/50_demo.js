6bca6cafb5bbd726fe8c0f6f1afff1fbcca5e9ab
Dayo Akanji <dakanji@user.noreply.github.com>
2016-06-24 21:50:47 +0300

    Update demo js

--- src/assets/js/demopage/50_demo.js
+++ src/assets/js/demopage/50_demo.js
@@ -37,20 +37,6 @@
 		e.preventDefault();
 	});
 	
-	var $button = $("<div id='source-button' class='btn btn-primary btn-xs'>&lt; &gt;</div>").click(function(){
-		var html = $(this).parent().html();
-		html = cleanSource(html);
-		$("#source-modal pre").text(html);
-		$("#source-modal").modal();
-	});
-	
-	$(".bs-component").hover(function(){
-		$(this).append($button);
-		$button.show();
-		}, function(){
-			$button.hide();
-		});
-		
 	enquire
 		.register("screen and (min-width:992px)", {
 			match: function() {
