9af89831f28647d18cca44c5fac7adbfbb1ae19c
Dayo Akanji <dakanji@user.noreply.github.com>
2016-07-27 01:46:27 +0300

    Swap cssnano for cosmic as former is stripping out font faces

--- gulpfile.js
+++ gulpfile.js
@@ -51,7 +51,8 @@ const CLEAN_UP = [
 		['\n\n', '\n'],
 		[',/*', ',\n\n/*'],
 		[';/*', ';\n\n/*'],
-		[' )/*', ' )\n\n/*'],
+		[')/*', ')\n\n/*'],
+		['}/*', '}\n\n/*'],
 		[new RegExp( '( [a-zA-Z0-9_$] )' ) + '/*', '\n/*']
 	];	
 
@@ -225,7 +226,7 @@ GULP.task( 'sass:main:compile', function() {
 		.pipe( $.multistream.apply( undefined, dev_destination ) )
 		
 		// Minify main CSS file for production build
-		.pipe( $.cond( PRODUCTION, $.cssnano() ) )
+		.pipe( $.cond( PRODUCTION, $.cssmin() ) )
 		.pipe( $.cond( PRODUCTION, $.batchReplace( CLEAN_UP ) ) )
 		.pipe( $.cond( PRODUCTION, $.extname( '.min.css' ) ) )
 		.pipe( $.cond( PRODUCTION, $.multistream.apply( undefined, prod_destination ) ) );
@@ -273,7 +274,7 @@ GULP.task( 'sass:extra:compile', ['sass:demo:compile'], function( done ) {
 				GULP.dest( DIST_ADMIN_DEV + '/css' ) 
 			 ) ),
 		$.merge( app_extra, admin_extra )
-			.pipe( $.cond( PRODUCTION, $.cssnano() ) )
+			.pipe( $.cond( PRODUCTION, $.cssmin() ) )
 			.pipe( $.cond( PRODUCTION, $.concat( 'admin-extra.min.css' ) ) )
 			.pipe( $.cond( PRODUCTION, $.batchReplace( CLEAN_UP ) ) )
 			.pipe( $.cond( PRODUCTION, GULP.dest( DIST_ADMIN_PROD + '/css' ) ) ),
@@ -285,7 +286,7 @@ GULP.task( 'sass:extra:compile', ['sass:demo:compile'], function( done ) {
 				GULP.dest( DIST_CATALOG_DEV + '/css' ) 
 			 ) ),
 		$.merge( app_extra, catalog_extra )
-			.pipe( $.cond( PRODUCTION, $.cssnano() ) )
+			.pipe( $.cond( PRODUCTION, $.cssmin() ) )
 			.pipe( $.cond( PRODUCTION, $.concat( 'catalog-extra.min.css' ) ) )
 			.pipe( $.cond( PRODUCTION, $.batchReplace( CLEAN_UP ) ) )
 			.pipe( $.cond( PRODUCTION, GULP.dest( DIST_CATALOG_PROD + '/css' ) ) )
@@ -313,7 +314,7 @@ GULP.task( 'sass:fonts:compile', function( done ) {
 		.pipe( $.cond( !PRODUCTION, GULP.dest( DIST_DEMO_DEV + '/css' ) ) )
         
          // Minify app-fonts CSS file for production build
-		.pipe( $.cond( PRODUCTION, $.cssnano() ) )
+		.pipe( $.cond( PRODUCTION, $.cssmin() ) )
 		.pipe( $.cond( PRODUCTION, $.batchReplace( CLEAN_UP ) ) )
 		.pipe( $.cond( PRODUCTION, $.extname( '.min.css' ) ) )
 		.pipe( $.cond( PRODUCTION, GULP.dest( DIST_ADMIN_PROD + '/css' ) ) )
@@ -494,7 +495,7 @@ GULP.task( 'copy:plugins', function() {
 				// Save to sources destination folder
 				.pipe( GULP.dest( DIST_ADMIN_SRC + '/plugins/select2' ) )
 				// Minify and save to production destination folder if this is a production run
-				.pipe( $.cond( PRODUCTION, $.cssnano() ) )
+				.pipe( $.cond( PRODUCTION, $.cssmin() ) )
 				.pipe( $.cond( PRODUCTION, $.batchReplace( CLEAN_UP ) ) )
 				.pipe( $.cond( PRODUCTION, $.extname( '.min.css' ) ) )
 				.pipe( GULP.dest( DIST_ADMIN_PROD + '/plugins/select2' ) ),
@@ -515,7 +516,7 @@ GULP.task( 'copy:plugins', function() {
 				// Save to sources destination folder
 				.pipe( GULP.dest( DIST_ADMIN_SRC + '/plugins/daterangepicker' ) )
 				// Minify and save to production destination folder if this is a production run
-				.pipe( $.cond( PRODUCTION, $.cssnano() ) )
+				.pipe( $.cond( PRODUCTION, $.cssmin() ) )
 				.pipe( $.cond( PRODUCTION, $.batchReplace( CLEAN_UP ) ) )
 				.pipe( $.cond( PRODUCTION, $.extname( '.min.css' ) ) )
 				.pipe( GULP.dest( DIST_ADMIN_PROD + '/plugins/daterangepicker' ) ),
@@ -546,7 +547,7 @@ GULP.task( 'copy:plugins', function() {
 				// Save to sources destination folder
 				.pipe( GULP.dest( DIST_ADMIN_SRC + '/plugins/gridstack' ) )
 				// Minify and save to production destination folder if this is a production run
-				.pipe( $.cond( PRODUCTION, $.cssnano() ) )
+				.pipe( $.cond( PRODUCTION, $.cssmin() ) )
 				.pipe( $.cond( PRODUCTION, $.batchReplace( CLEAN_UP ) ) )
 				.pipe( $.cond( PRODUCTION, $.extname( '.min.css' ) ) )
 				.pipe( GULP.dest( DIST_ADMIN_PROD + '/plugins/gridstack' ) )
